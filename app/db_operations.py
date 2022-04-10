fe63c70c89bdf59b3edd6a630562b4b0c4f11d8c
amrit96 <banerjee96.amritayan@gmail.com>
2022-04-10 12:33:59 +0530

    update get filter

--- app/db_operations.py
+++ app/db_operations.py
@@ -14,17 +14,18 @@ def add_data(table_name, data_obj):
         else:
             session.commit()
 
-def get_data(table_name, condition=None):
 
+def get_data(table_name, filter_key=None, filter_value=None):
     with Session(engine) as session:
-        if condition:
-            data = session.query(table_name).filter_by(condition).all()
+        if filter_key:
+            data = session.query(table_name).filter(getattr(table_name, filter_key).like(filter_value)).all()
         else:
             data = session.query(table_name).all()
     data = [row.__dict__ for row in data]
     for row in data:
         del row['_sa_instance_state']
     return data
+
 
 def update_data(table_name, data_object, filter_value):
     with Session(engine) as session:
@@ -34,7 +35,8 @@ def update_data(table_name, data_object, filter_value):
         except Exception as e:
             print(e)
 
-def delete_data(table_name,filter_value):
+
+def delete_data(table_name, filter_value):
     with Session(engine) as session:
         session.begin()
         try:
@@ -44,15 +46,16 @@ def delete_data(table_name,filter_value):
             session.rollback()
             raise
 
+
 # if __name__ == "__main__":
-#     from models import Continent, Country, City
-#     data = get_data(table_name=Continent)
-#     # data[0]['population'] = 1001
-#     # update_data(table_name=Continent, data_object={Continent.population: 2001}, filter_value=1)
-#     # data = get_data(table_name=Continent)
-#     data2 = Continent(name='North America', population=2, area=102.5)
-#     add_data(table_name=Continent, data_obj=data2)
-#     data = get_data(table_name=Continent)
-#     delete_data(Continent, filter_value=1)
-#     data = get_data(table_name=Continent)
-#     print(data)
+    # from models import Continent, Country, City
+    # data = get_data(table_name=Continent)#, filter_key='population',filter_value=2)
+    #     # data[0]['population'] = 1001
+    # update_data(table_name=Continent, data_object={Continent.population: 2001}, filter_value=1)
+    # data = get_data(table_name=Continent)
+    #     data2 = Continent(name='North America', population=2, area=102.5)
+    #     add_data(table_name=Continent, data_obj=data2)
+    #     data = get_data(table_name=Continent)
+    # delete_data(Continent, filter_value=2)
+    #     data = get_data(table_name=Continent)
+    # print(data)
