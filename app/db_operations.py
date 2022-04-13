dde5c24248eb86962d554e91f336e2b2647e0c94
amrit96 <banerjee96.amritayan@gmail.com>
2022-04-13 16:24:58 +0530

    added docstring

--- app/db_operations.py
+++ app/db_operations.py
@@ -5,6 +5,14 @@ from tasks import app
 
 @app.task
 def add_data(data_obj):
+    """
+    this method will be used to insert new data tot the db
+    Args:
+        data_obj: object of the orm class
+
+    Returns:
+
+    """
     with Session(engine) as session:
         session.begin()
         try:
@@ -19,6 +27,16 @@ def add_data(data_obj):
 
 @app.task
 def get_data(table_name, filter_key=None, filter_value=None):
+    """
+    this method will be used to fetch data of a particular table from the db
+    Args:
+        table_name: name of the table from which data is to be fetched
+        filter_key: name of the column against which the filtering is to be done
+        filter_value: value of the filtering column
+
+    Returns:
+        a list of dictionary where each dictionary represents a row
+    """
     with Session(engine) as session:
         if filter_key:
             data = session.query(table_name).filter(getattr(table_name, filter_key).like(filter_value)).all()
@@ -32,6 +50,16 @@ def get_data(table_name, filter_key=None, filter_value=None):
 
 @app.task
 def update_data(table_name, data_object, id_value):
+    """
+    this method will be used to update an existing record
+    Args:
+        table_name: name of the table in which the data is to be updated
+        data_object: a dict containing the key value pair(s) of th updated data
+        id_value: the primary key (id) of the row where the data is to be altered
+
+    Returns:
+
+    """
     with Session(engine) as session:
         try:
             session.query(table_name).filter_by(id=id_value).update(data_object)
@@ -42,6 +70,15 @@ def update_data(table_name, data_object, id_value):
 
 @app.task
 def delete_data(table_name, id_value):
+    """
+    this method will me used to remove data from db table
+    Args:
+        table_name: name of the table from where data is to be removed
+        id_value: the primary key (id) of the row which is to be removed
+
+    Returns:
+
+    """
     with Session(engine) as session:
         session.begin()
         try:
